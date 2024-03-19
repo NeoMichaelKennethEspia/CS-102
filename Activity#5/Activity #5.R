5ea38e15d3ccdc6f6304165695edd3202a7347e9
Joshua Somosera <joshua.somosera@students.isatu.edu.ph>
2024-03-19 13:58:26 +0000

    (Update) Activity#5

--- Activity#5/Activity #5.R
+++ Activity#5/Activity #5.R
@@ -1,13 +1,14 @@
-install.packages("dplyr")
 library(dplyr)
-
-data <- read_csv("Activity#3/[CSV Files]/FinalMovie.csv")
-data    
-
-# Ratings 
-  Rate <- data %>% filter(Rating < 9)
-  Rate
-  
-head(Rate)
+library(readr)
 
 
+cleaned <- read_csv("Activity#3/[CSV Files]/FinalMovie.csv")
+str(cleaned)
+
+cleaned_data <- cleaned %>% na.omit() %>%
+mutate_all(str_trim)
+
+str(cleaned_data)
+
+write.csv(cleaned_data, "Cleaned_Data.csv", row.names = FALSE)
+View(Cleaned_Data)
