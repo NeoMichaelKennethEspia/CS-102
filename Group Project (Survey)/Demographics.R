77f91133dcc82b4c5f30ca2303bda6f96dc9ef6f
Joshua Somosera <joshua.somosera@students.isatu.edu.ph>
2024-04-24 14:49:31 +0000

    Commit 1

--- Group Project (Survey)/Demographics.R
+++ Group Project (Survey)/Demographics.R
@@ -1,10 +1,40 @@
 surveydata <- read_csv("Survey Data/surveydata.csv")
 
 #Factor Gender
-surveydata$gender<-factor(surveydata$`Gender:`, levels = c("Straight", "Bisexual","Gay","Lesbian","Transgender","Non-binary/non-conforming","Prefer Not to Say"))
-summary(surveydata$gender)
+genderfactor<-factor(surveydata$`Gender:`, levels = c("Straight", "Bisexual","Gay","Lesbian","Transgender","Non-binary/non-conforming","Prefer Not to Say"))
+summary(genderfactor)
 
-gender_summary <- table(surveydata$gender)
+gender_summary <- table(genderfactor)
+
+#Factor Sex
+sexfactor<-factor(surveydata$`SEX:`, levels = c("Male", "Female"))
+summary(sexfactor)
+
+sex_summary <- table(sexfactor)
+
+
+#Factor Age 
+agefactor<-factor(surveydata$`Age:`, levels = c(11,12,13,14,15,16,17,18,19,20,21,22,23))
+summary(agefactor)
+
+age_summary <- table(agefactor)
+
+#Cleaning School Data
+schooldata <-surveydata$`School Name:`
+
+
+school<- gsub("Iloilo Scienc and Technology University| Iloilo science and technology University |Iloilo Science and Technology University | ILOILO SCIENCE AND TECHNOLOGY UNIVERSITY |ISATU |ISATU MAIN  ", "Iloilo Science and Technology University", surveydata$`School Name:`)
+school
+
+
+
+#Factor
+schooldata<-factor(schooldata)
+summary(schooldata)
+
+
+
+
 
 #Graph for Gender
 barplot(gender_summary, 
@@ -17,12 +47,6 @@ barplot(gender_summary,
         space = 0.5
 )
 
-#Factor Sex
-surveydata$sex<-factor(surveydata$`SEX:`, levels = c("Male", "Female"))
-summary(surveydata$sex)
-
-sex_summary <- table(surveydata$sex)
-
 #Graph for Sex
 barplot(sex_summary, 
         main = "Sex Distribution", 
@@ -34,12 +58,6 @@ barplot(sex_summary,
         space = 0.5
 )
 
-#Factor Age 
-surveydata$age<-factor(surveydata$`Age:`, 11:23,  levels = c(11,12,13,14,15,16,17,18,19,20,21,22,23))
-summary(surveydata$age)
-
-age_summary <- table(surveydata$age)
-
 #Graph for Age
 barplot(age_summary, 
         main = "Age Distribution", 
@@ -50,14 +68,3 @@ barplot(age_summary,
         ylim = c(0, max(age_summary) * 1.1), 
         space = 0.5
 )
-
-#Cleaning School Data
-schooldata <-surveydata$`School Name:`
-
-#Factor
-schooldata<-factor(schooldata)
-summary(schooldata)
-
-
-school<- gsub("Iloilo Scienc and Technology University| Iloilo science and technology University |Iloilo Science and Technology University | ILOILO SCIENCE AND TECHNOLOGY UNIVERSITY |ISATU |ISATU MAIN  ", "Iloilo Science and Technology University", surveydata$`School Name:`)
-school
